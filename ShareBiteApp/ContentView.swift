da688ea530d4078e4f1f97d869e5272943a43bf2
krupamaisuria <krupamaisuria94@gmail.comgit config user.email krupamaisuria94@gmail.com>
2024-08-25 23:19:18 -0400

    prevent session

--- ShareBiteApp/ContentView.swift
+++ ShareBiteApp/ContentView.swift
@@ -3,31 +3,25 @@ import Firebase
 
 struct ContentView: View {
     @State private var showSplashScreen = false
-    @State private var isShowingSignUP = false
-    @State private var alertMessage = ""
-    @State private var isShowingAlert = false
-    @State private var isAuthenticated = false
-    @State private var userEmail = ""
-    @State private var email : String = ""
-    @State private var password : String = ""
     @State private var selectedTab = 0
+    @StateObject private var sessionManager = SessionManager.shared
+    
     var body: some View {
-        NavigationView{
+        NavigationView {
             VStack {
-                if isAuthenticated {
-                                  HeaderView()
-                                    Divider().background(Color.blue)
-                                BottomNavigationView(selectedTab : selectedTab)
-                                  
-                                } else {
-                                    SignInView()
-                                        .opacity(showSplashScreen ? 0 : 1)
-                                }
-                                if showSplashScreen {
-                                    SplashScreenView()      
-                                        .transition(.opacity)
-                                }
+                if sessionManager.isLoggedIn {
+                    HeaderView()
+                    Divider().background(Color.blue)
+                    BottomNavigationView(selectedTab: selectedTab)
+                } else {
+                    SignInView()
+                        .opacity(showSplashScreen ? 0 : 1)
+                }
                 
+                if showSplashScreen {
+                    SplashScreenView()
+                        .transition(.opacity)
+                }
             }
             .background(Color.white.edgesIgnoringSafeArea(.all))
             .onAppear {
@@ -38,26 +32,28 @@ struct ContentView: View {
                 }
                 checkAuthentication()
             }
-           
-            
         }
     }
+    
     private func checkAuthentication() {
-            Auth.auth().addStateDidChangeListener { auth, user in
-                if let _ = user {
-                    
-                    isAuthenticated = true
-                } else {
-                    
-                    isAuthenticated = false
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if let user = user {
+                sessionManager.loginUser(userid: user.uid) { success in
+                    // Handle successful login
+                    print("User logged in: \(success)")
                 }
+            } else {
+                // Handle user not logged in
+                print("No user logged in")
+                sessionManager.logoutUser()
             }
         }
-   
+    }
 }
 
-struct ContentView_Preview : PreviewProvider {
+struct ContentView_Preview: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environment(\.colorScheme, .light) // Add environment settings if needed
     }
 }
